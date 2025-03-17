693f09e94b4c9ba8c51abbc95381ee6425597842
Aurélian HAFFNER <97893599+Jin1411@users.noreply.github.com>
2025-03-17 16:16:26 -0400

    feat : added score display

--- BaseBallRV/Assets/Scripts/ScoreManager.cs
+++ BaseBallRV/Assets/Scripts/ScoreManager.cs
@@ -1,22 +1,43 @@
 using System.Collections;
+using TMPro;
 using UnityEngine;
 
 public class ScoreManager : MonoBehaviour
 {
 
+    [SerializeField] private TMP_Text _scoreDisplay;
+    [SerializeField] private int _maxDisplayedScore = 1000000;
     private int score = 0;
+
+    private void Start()
+    {
+        
+    }
 
     public void addScore(int score)
     {
         this.score += score;
+        UpdateUI();
     }
 
     public int getScore() {  return score; }
 
-    public void removeScore(int score) {  this.score -= score; }
+    public void removeScore(int score) {  
+        this.score -= score;
+        UpdateUI();
+    }
 
     public void Reset()
     {
-        score = 0; 
+        score = 0;
+        UpdateUI();
+    }
+
+    private void UpdateUI()
+    {
+        if (_scoreDisplay != null) {
+            _scoreDisplay.text = (this.score % _maxDisplayedScore).ToString();
+        }
+        
     }
 }
