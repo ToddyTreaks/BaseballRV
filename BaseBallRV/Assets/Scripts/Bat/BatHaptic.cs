17dd390bac1aaed3b169e31db9722c501bce2dc3
Aurélian HAFFNER <97893599+Jin1411@users.noreply.github.com>
2025-04-18 11:24:16 -0400

    feat : added haptic feedback to bat collision

---
+++ BaseBallRV/Assets/Scripts/Bat/BatHaptic.cs
@@ -0,0 +1,49 @@
+using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+using UnityEngine.XR.Interaction.Toolkit.Interactables;
+using UnityEngine.XR.Interaction.Toolkit.Interactors;
+
+[RequireComponent(typeof(XRBaseInteractable))]
+public class BatHaptic : MonoBehaviour
+{
+    public float intensityFactor = 1.0f;
+    public float duration = 0.1f;  // seconds
+
+    private XRBaseInteractable interactable;
+    private XRBaseInputInteractor controller;
+
+    void Awake()
+    {
+        interactable = GetComponent<XRBaseInteractable>();
+        interactable.selectEntered.AddListener(OnSelectEntered);
+        interactable.selectExited.AddListener(OnSelectExited);
+    }
+        void OnDestroy()
+    {
+        interactable.selectEntered.RemoveListener(OnSelectEntered);
+        interactable.selectExited.RemoveListener(OnSelectExited);
+    }
+
+    private void OnSelectEntered(SelectEnterEventArgs args)
+    {
+        var interactor = args.interactorObject;
+
+        // Try to get XRBaseControllerInteractor
+        if (interactor is UnityEngine.XR.Interaction.Toolkit.Interactors.XRBaseInputInteractor controllerInteractor)
+        {
+            this.controller = controllerInteractor;
+        }
+    }
+
+    private void OnSelectExited(SelectExitEventArgs args)
+    {
+        this.controller = null;
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (this.controller != null && collision.collider.gameObject != this.controller.gameObject) {
+            this.controller.SendHapticImpulse(collision.impulse.magnitude * intensityFactor, this.duration);
+        }
+    }
+}
