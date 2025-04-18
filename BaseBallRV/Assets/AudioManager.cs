f9a183bdc60532a252a8234fc08e6eb2de6319c6
Thomien Leygonie <124178882+ToddyTreaks@users.noreply.github.com>
2025-04-18 12:39:22 -0400

    add audio manager

---
+++ BaseBallRV/Assets/AudioManager.cs
@@ -0,0 +1,42 @@
+using System;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    public static AudioManager Instance { get; private set; }
+
+    public Sound[] ambientSounds, sfxSounds;
+    public AudioSource ambientSource, sfxSource;
+
+    private void Awake()
+    {
+        if(Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    public void PlayAmbient(string name)
+    {
+        Sound s = Array.Find(ambientSounds, x => x.name == name);
+        if (s != null)
+        {
+            ambientSource.clip = s.clip;
+            ambientSource.Play();
+        }
+    }
+
+    public void PlaySFX(string name)
+    {
+        Sound s = Array.Find(sfxSounds, x => x.name == name);
+        if (s != null)
+        {
+            sfxSource.PlayOneShot(s.clip);
+        }
+    }
+}
